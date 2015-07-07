3f339e6f802100cf3da879f54d97f0adfcd9a8e4
Vedmax <max.vedernikov@gmail.com>
2015-07-07 20:31:59 +0500

    Add figure on the board

--- Chess/Chess/Scripts/Canvas/field.js
+++ Chess/Chess/Scripts/Canvas/field.js
@@ -3,31 +3,50 @@
 var field;
 var ctxField;
 
+var fig;
+var ctxFig;
+
 var fieldWidth = 800;
 var fieldHeight = 800;
 
-var background = new Image();
-background.src = "../Content/pole.jpg";
+var board = new Image();
+board.src = "../Content/board2.jpg";
+
+var p_figure = new Image();
+p_figure.src = "../Content/Bishop_White.gif";
+
+function Figure() {
+	this.srcX = 0;
+	this.srcY = 0;
+	this.drawX = 45;
+	this.drawY = 45;
+	this.width = 48;
+	this.height = 48;
+}
+
+Figure.prototype.draw = function ()
+{
+	ctxField.drawImage(p_figure, this.srcX, this.srcY, this.width, this.height,
+		this.drawX, this.drawY, 89, 89);
+	console.log("kjsjdfkjf");
+}
 
 function init() {
 	field = document.getElementById("field");
 	ctxField = field.getContext("2d");
-
+	
 	field.width = fieldWidth;
 	field.height = fieldHeight;
+
+	var figure = new Figure();
+
 	drawBg();
+	figure.draw();
 }
 
-function drawRectangles() {
-	ctxField.fillStyle = "#f0f";
-	for (var i = 0; i < 8; i++) {
-		for (var j = 0; j < 8; j++) {
-			ctxField.fillRect(10*i, 10*j, 10, 10);
-		}
-	}
-}
+
 
 function drawBg() {
-	ctxField.drawImage(background, 0, 0, 503, 480,
+	ctxField.drawImage(board, 0, 0, 800, 800,
 		0, 0, fieldWidth, fieldHeight);
 }
